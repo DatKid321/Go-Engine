1a6732e2e8721484093aa1ff2337bace6512da68
DatKid321 <139932883+DatKid321@users.noreply.github.com>
2023-12-18 20:29:26 +1100

    test
    test

--- load_game.py
+++ load_game.py
@@ -1,1 +1,20 @@
-#To load game
+import pygame
+pygame.init()
+
+with open("-1.sgf", "r") as sgf_file:
+    print("hi")
+    for line in sgf_file:
+        print(line)
+
+screen = pygame.display.set_mode([500, 500])
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((255, 204, 170))
+    pygame.display.flip()
+
+pygame.quit()
+
