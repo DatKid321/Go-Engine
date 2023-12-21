1be92d1f1cd687742cf0ee39e599298c92b9f982
DatKid321 <139932883+DatKid321@users.noreply.github.com>
2023-12-21 17:48:47 +1100

    cont.

--- load_game.py
+++ load_game.py
@@ -1,18 +1,23 @@
 import pygame
 
+Colour = {"B" : (0, 0, 0), "W" : (255, 255, 255)}
+
 pygame.init()
 
 screen = pygame.display.set_mode([500, 500])
 screen.fill((220, 177, 108))
 
 for i in range(1, 20):
-    pygame.draw.line(screen, (0, 0, 0), (25*i, 25*1), (25*i, 25*19))
-    pygame.draw.line(screen, (0, 0, 0), (25*1, 25*i), (25*19, 25*i))
+    pygame.draw.line(screen, Colour["B"], (25 * i, 25 * 1), (25 * i, 25 * 19))
+    pygame.draw.line(screen, Colour["B"], (25 * 1, 25 * i), (25 * 19, 25 * i))
 
 with open("-1.sgf", "r") as sgf_file:
-    print("hi")
     for line in sgf_file:
-        print(line)
+        for move in filter(None, line.strip().split(";")[1:]):
+            position = (25 * (ord(move[2]) - 96), 25 * (ord(move[3]) - 96))
+            pygame.draw.circle(screen, Colour[move[0]], position, 12)
+            pygame.draw.circle(screen, Colour["B"], position, 12, 1)
+
 
 pygame.display.flip()
 
