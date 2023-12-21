36c39f16f7f3ebe377913b5ffbb90fb8e04c8241
DatKid321 <139932883+DatKid321@users.noreply.github.com>
2023-12-21 16:42:48 +1100

    board

--- load_game.py
+++ load_game.py
@@ -1,20 +1,26 @@
 import pygame
+
 pygame.init()
+
+screen = pygame.display.set_mode([500, 500])
+screen.fill((220, 177, 108))
+
+for i in range(1, 20):
+    pygame.draw.line(screen, (0, 0, 0), (25*i, 25*1), (25*i, 25*19))
+    pygame.draw.line(screen, (0, 0, 0), (25*1, 25*i), (25*19, 25*i))
 
 with open("-1.sgf", "r") as sgf_file:
     print("hi")
     for line in sgf_file:
         print(line)
 
-screen = pygame.display.set_mode([500, 500])
+pygame.display.flip()
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((255, 204, 170))
-    pygame.display.flip()
 
 pygame.quit()
 
