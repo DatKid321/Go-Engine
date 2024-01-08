592d5e2dad866ff38d6e501b8b55604a594b9d55
DatKid321 <139932883+DatKid321@users.noreply.github.com>
2024-01-08 18:31:57 +1100

    Update load_game.py

--- load_game.py
+++ load_game.py
@@ -1,40 +1,57 @@
-import itertools as iter
+import itertools as it
 import numpy as np
 import pygame as pg
 
-Colour = {"B": (0, 0, 0), "W": (255, 255, 255), "G": (242, 176, 109)}
-Goban = np.zeros((19, 19), dtype = 'bool')
-maskedGoban = np.ma.masked_array(Goban, mask = np.ones((19, 19)))
+Colour = {True: (0, 0, 0), False: (255, 255, 255), None: (242, 176, 109)}
+maskedGoban = np.ma.array(np.empty((19, 19)), dtype = 'bool', mask = True)
 
-def px(*ints):
-    return 25 * np.array(ints)
-
-print(px(1,2), 'hello')
-print([point for point in iter.product(px(4, 10, 16), repeat = 2)])
+with open("-1.sgf", "r") as sgf_file:
+    moves = iter([move for line in sgf_file for move in filter(None, line.strip().split(";")[1:])]) 
 
 pg.init()
 
 screen = pg.display.set_mode([500, 500])
-screen.fill(Colour["G"])
+screen.fill(Colour[None])
 
-for point in iter.product(px(4, 10, 16), repeat = 2): pg.draw.circle(screen, Colour["B"], point, 5)
-for i in range(1, 20):
-    pg.draw.line(screen, Colour["B"], px(i, 1), px(i, 19))
-    pg.draw.line(screen, Colour["B"], px(1, i), px(19, i))
+def px(*ints):
+    return 25 * (np.array(ints) + 1)
 
-with open("-1.sgf", "r") as sgf_file:
-    for line in sgf_file:
-        for move in filter(None, line.strip().split(";")[1:]):
-            # Goban[ord(move[2]) - 96][ord(move[3]) - 96] += 1
-            position = (25 * (ord(move[2]) - 96), 25 * (ord(move[3]) - 96))
-            pg.draw.circle(screen, Colour[move[0]], position, 12)
-            pg.draw.circle(screen, Colour["B"], position, 12, 1)
+def drawStones():
+    for point in it.product(range(19), repeat = 2):
+        try:
+            pg.draw.circle(screen, Colour[maskedGoban[point]], px(*point), 12)
+            pg.draw.circle(screen, Colour[True], px(*point), 12, 1)
+        except TypeError:
+            pass
+    pg.display.flip()
+
+def nextMove():
+    try:
+        move = next(moves)
+        position = (ord(move[2]) - 97, ord(move[3]) - 97)
+        maskedGoban[position] = (move[0] == "B")
+        drawStones()
+    except StopIteration:
+        pass
+
+
+for point in it.product(px(3, 9, 15), repeat = 2): pg.draw.circle(screen, Colour[True], point, 5)
+for i in range(19):
+    pg.draw.line(screen, Colour[True], px(i, 0), px(i, 18))
+    pg.draw.line(screen, Colour[True], px(0, i), px(18, i))
+    
+
+drawStones()
+print(maskedGoban)
 
 pg.display.flip()
 
 running = True
 while running:
     for event in pg.event.get():
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                nextMove()
         if event.type == pg.QUIT:
             running = False
 
