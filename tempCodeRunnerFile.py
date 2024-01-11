5dbdab2d6add03862283c8d68b3769c6df2ae7c9
DatKid321 <139932883+DatKid321@users.noreply.github.com>
2024-01-11 23:38:05 +1100

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-            # if isinstance(maskedGoban[tuple(adjacent)], np.bool_) and (maskedGoban[tuple(position)] != maskedGoban[tuple(adjacent)]) and libertyCount(not maskedGoban[tuple(position)], adjacent):
-            #     maskedGoban.mask = oldMask
